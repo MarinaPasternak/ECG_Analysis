c7546de5ee57294caf896feddf0e5d9829e45a76
Marina Pasternak <256pasternakmarina@gmail.com>
2021-05-16 16:15:30 +0300

    added Haar wavlet

--- Analyz_EKG/Analyz_EKG/Form1.Designer.cs
+++ Analyz_EKG/Analyz_EKG/Form1.Designer.cs
@@ -29,30 +29,37 @@ namespace Analyz_EKG
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.generateDataBtn = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
             // 
             // chart1
             // 
-            chartArea1.AlignmentOrientation = ((System.Windows.Forms.DataVisualization.Charting.AreaAlignmentOrientations)((System.Windows.Forms.DataVisualization.Charting.AreaAlignmentOrientations.Vertical | System.Windows.Forms.DataVisualization.Charting.AreaAlignmentOrientations.Horizontal)));
-            chartArea1.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.chart1.Legends.Add(legend1);
+            chartArea2.AlignmentOrientation = ((System.Windows.Forms.DataVisualization.Charting.AreaAlignmentOrientations)((System.Windows.Forms.DataVisualization.Charting.AreaAlignmentOrientations.Vertical | System.Windows.Forms.DataVisualization.Charting.AreaAlignmentOrientations.Horizontal)));
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
             this.chart1.Location = new System.Drawing.Point(253, 27);
             this.chart1.Margin = new System.Windows.Forms.Padding(4);
             this.chart1.Name = "chart1";
-            series1.ChartArea = "ChartArea1";
-            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
-            series1.Legend = "Legend1";
-            series1.Name = "Series1";
-            this.chart1.Series.Add(series1);
-            this.chart1.Size = new System.Drawing.Size(626, 198);
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(607, 198);
             this.chart1.TabIndex = 0;
             this.chart1.Text = "chart1";
             // 
@@ -67,11 +74,62 @@ namespace Analyz_EKG
             this.generateDataBtn.UseVisualStyleBackColor = true;
             this.generateDataBtn.Click += new System.EventHandler(this.generateDataBtn_Click_1);
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(877, 49);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(134, 593);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(1054, 53);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(134, 593);
+            this.dataGridView2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(873, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 19);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Оригінальні дані";
+            // 
+            // label2
+            // 
+            this.label2.AllowDrop = true;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1027, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(192, 19);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Після вейвлет перетвореня";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(25, 87);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(207, 39);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Вейвлет перетворення";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1200, 658);
+            this.ClientSize = new System.Drawing.Size(1219, 658);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.generateDataBtn);
             this.Controls.Add(this.chart1);
             this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
@@ -79,7 +137,10 @@ namespace Analyz_EKG
             this.Name = "Form1";
             this.Text = "Analyz_EKG";
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -87,6 +148,11 @@ namespace Analyz_EKG
 
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
         private System.Windows.Forms.Button generateDataBtn;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Analyz_EKG/Analyz_EKG/Form1.cs
+++ Analyz_EKG/Analyz_EKG/Form1.cs
@@ -8,22 +8,28 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using Accord.Math;
+using Accord.Math.Wavelets;
+using Accord.Imaging.Filters;
+using System.Numerics;
 
 namespace Analyz_EKG
 {
     public partial class Form1 : Form
     {
-        string csv_file_path = @"C:\Users\Marina\Desktop\диплом\Analyz_EKG\data.csv";
+        string csv_file_path = @"C:\Users\Marina\Desktop\диплом\ECG_Analysis\Analyz_EKG\data.csv";
         int[] dataECG;
-        int[] dataForExamination;
+        double[] dataForExamination;
         int[] step;
+
 
         public Form1()
         {
             InitializeComponent();
         }
 
-        public int[] readECGData() {
+        public int[] readECGData() 
+        {
             int[] readRezults;
             string[] stringSeparators = new string[] { "\r\n" };
 
@@ -31,11 +37,13 @@ namespace Analyz_EKG
             string data = readfile.ReadToEnd();
             readRezults = data.Split(stringSeparators, StringSplitOptions.None).Select(x => int.Parse(x)).ToArray();
 
+            
             return readRezults;
         }
 
-        public int findMin(int[] numsArray) {
-            int min = numsArray[0];
+        public double findMin(double[] numsArray)
+        {
+            double min = numsArray[0];
             int lengthArray = numsArray.Length;
             int i = 0;
 
@@ -50,6 +58,13 @@ namespace Analyz_EKG
             return min;
         }
 
+        public double findSqtwolog() 
+        {
+            double T = 0;
+
+            return T;
+        }
+
 
         private void generateDataBtn_Click_1(object sender, EventArgs e)
         {
@@ -57,24 +72,57 @@ namespace Analyz_EKG
             int j = 0;
             int a = randomNum.Next(0, 94500);
             int b = a + 500;
+            int i = 0;
 
-            dataForExamination = new int[b - a];
+            dataGridView1.RowCount = b - a;
+            dataGridView1.ColumnCount = 1;
+
+            dataForExamination = new double[b - a];
+            
+
             step = new int[b - a];
             
-            for (int i = a; i < b; i++) {
+            for (i = a; i < b; i++)
+            {
                 step[j] = i;
                 j++;
             }
             
             dataECG = readECGData();
             Array.Copy(dataECG, a, dataForExamination, 0, b - a);
+            
 
-            int minValue = findMin(dataForExamination);
+            for (i = 0; i < b-a; i++) 
+            {
+                dataGridView1.Rows[i].Cells[0].Value = dataForExamination[i];
+            }
+
+            double minValue = findMin(dataForExamination); 
 
             chart1.ChartAreas[0].AxisY.Minimum = minValue;
             chart1.ChartAreas[0].AxisX.ScrollBar.IsPositionedInside = true;
             chart1.Series[0].Points.DataBindXY(step, dataForExamination);
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            double[] waveletArray = new double[dataForExamination.Length];
+
+            dataGridView2.RowCount = dataForExamination.Length;
+            dataGridView2.ColumnCount = 1;
+
+            Array.Copy(dataForExamination, 0, waveletArray, 0, dataForExamination.Length);
+
+            IWavelet wavelet = new Accord.Math.Wavelets.Haar(4);
+            WaveletTransform target = new WaveletTransform(wavelet);
+            wavelet.Forward(waveletArray);
+
+            for (i = 0; i < waveletArray.Length; i++)
+            {
+                dataGridView2.Rows[i].Cells[0].Value = waveletArray[i];
+            }
         }
     }
 }
